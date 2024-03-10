be46a98874b77f20838bcc36a875e9e606934018
DavuluriGayatri <gayatridavuluri147@gmail.com>
2024-03-10 09:01:26 -0400

    User input function and Map creation
    Adjusted code with actual map given in the problem and created user input function for start and goal node.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Dijkstra_algorithm
+ enpm661_project2
--- dijkstra_gayatri_davuluri.py
+++ dijkstra_gayatri_davuluri.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 # Constants
-CLEARANCE = 5
-ROBOT_RADIUS = 0
 MAP_HEIGHT = 500
 MAP_WIDTH = 1200
+CLEARANCE = 5
+ROBOT_RADIUS = 0
 
 # Actions defined as below
 actions = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, 1), (1, -1), (-1, -1)]
@@ -16,14 +16,24 @@ actions = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, 1), (1, -1), (-1, -1)]
 def apply_action(node, action):
     return tuple(np.add(node, action))
 
-# Map creation using OpenCV
-def create_map():
+def create_map_canvas():
     canvas = np.zeros((MAP_HEIGHT, MAP_WIDTH, 3), dtype=np.uint8)
+
+    # Obstacle 1 (rectangle)
     cv2.rectangle(canvas, (100 + CLEARANCE, 100 + CLEARANCE), (175 - CLEARANCE, 500 - CLEARANCE), (255, 255, 255), thickness=cv2.FILLED)
+    # Obstacle 2 (rectangle)
     cv2.rectangle(canvas, (275 + CLEARANCE, 0 + CLEARANCE), (350 - CLEARANCE, 400 - CLEARANCE), (255, 255, 255), thickness=cv2.FILLED)
+    # Obstacle 3 (Hexagon)
+    hexagon_pts = np.array([[650, 120], [537, 185], [537, 315], [650, 380], [763, 315], [763, 185]], dtype=np.int32)
+    cv2.fillPoly(canvas, [hexagon_pts], color=(255, 255, 255))
+    # Obstacle 4 (Rectangular C-shaped obstacle)
+    c_shape_rect_pts = np.array([[900, 450], [1100, 450], [1100, 50], [900, 50], [900, 125], [1020, 125], [1020, 375], [900, 375]], dtype=np.int32)
+    cv2.fillPoly(canvas, [c_shape_rect_pts], color=(255, 255, 255))
+    cv2.polylines(canvas, [c_shape_rect_pts], isClosed=True, color=(255, 255, 255), thickness=1)
+
     return canvas
 
-def is_valid(node):
+def is_valid(node, map_canvas):
     x, y = node
     if x < 0 or x >= MAP_WIDTH or y < 0 or y >= MAP_HEIGHT:
         return False
@@ -33,6 +43,7 @@ class PriorityQueueNode:
     def __init__(self, cost, node):
         self.cost = cost
         self.node = node
+
     def __lt__(self, other):
         return self.cost < other.cost
 
@@ -47,17 +58,17 @@ def backtrack(parents, start, goal):
 def cost(node1, node2):
     return np.sqrt((node1[0] - node2[0])**2 + (node1[1] - node2[1])**2)
 
-def generate_graph(start, goal):
+def generate_graph(start, goal, map_canvas):
     open_list = []
     heapq.heappush(open_list, PriorityQueueNode(0, start))
     closed_list = set()
-    exploration = []  
+    exploration = []
     parents = {}
     cost_to_come = {start: 0}
 
     while open_list:
         current_node = heapq.heappop(open_list).node
-        exploration.append(current_node)  
+        exploration.append(current_node)
 
         if current_node == goal:
             return backtrack(parents, start, goal), exploration
@@ -66,44 +77,43 @@ def generate_graph(start, goal):
 
         for action in actions:
             new_node = apply_action(current_node, action)
-            if new_node not in closed_list and is_valid(new_node):
+            if new_node not in closed_list and is_valid(new_node, map_canvas):
                 new_cost = cost_to_come[current_node] + cost(current_node, new_node)
                 if new_node not in cost_to_come or new_cost < cost_to_come[new_node]:
                     cost_to_come[new_node] = new_cost
                     heapq.heappush(open_list, PriorityQueueNode(new_cost, new_node))
                     parents[new_node] = current_node
 
-    return None, None
+    return None, exploration
 
-map_canvas = create_map()
+def get_user_input():
+    start_x = int(input("Enter the x-coordinate for the start node: "))
+    start_y = int(input("Enter the y-coordinate for the start node: "))
+    goal_x = int(input("Enter the x-coordinate for the goal node: "))
+    goal_y = int(input("Enter the y-coordinate for the goal node: "))
+    return (start_x, start_y), (goal_x, goal_y)
 
-start_x = int(input("Enter the x-coordinate for the start node: "))
-start_y = int(input("Enter the y-coordinate for the start node: "))
-start_node = (start_x, start_y)
+if __name__ == "__main__":
+    map_canvas = create_map_canvas()
+    start_node, goal_node = get_user_input()
 
-goal_x = int(input("Enter the x-coordinate for the goal node: "))
-goal_y = int(input("Enter the y-coordinate for the goal node: "))
-goal_node = (goal_x, goal_y)
+    # Generate Graph and Optimal Path
+    optimal_path, exploration = generate_graph(start_node, goal_node, map_canvas)
 
-# Generate Graph and Optimal Path
-optimal_path, exploration = generate_graph(start_node, goal_node)
+    # Visualization
+    fig, ax = plt.subplots(figsize=(10, 5))
+    ax.imshow(cv2.cvtColor(map_canvas, cv2.COLOR_BGR2RGB), extent=[0, MAP_WIDTH, 0, MAP_HEIGHT])
+    exploration_step_size = 10000
 
-# Visualization
-fig, ax = plt.subplots(figsize=(10, 5))
-ax.imshow(cv2.cvtColor(map_canvas, cv2.COLOR_BGR2RGB), extent=[0, MAP_WIDTH, 0, MAP_HEIGHT])  # Set extent for (0,0) as left lower corner
-exploration_step_size = 10000  
+    def animate(i):
+        explored_nodes = exploration[:i * exploration_step_size]
+        if explored_nodes:
+            ax.scatter([node[0] for node in explored_nodes], [node[1] for node in explored_nodes], color='yellow', s=1)
+        if i * exploration_step_size >= len(exploration) and optimal_path:
+            ax.scatter([node[0] for node in optimal_path], [node[1] for node in optimal_path], color='red', s=1)
 
-def animate(i):
-    explored_nodes = exploration[:i*exploration_step_size]
-    if explored_nodes:
-        ax.scatter([node[0] for node in explored_nodes], [node[1] for node in explored_nodes], color='yellow', s=1)
-    if i*exploration_step_size >= len(exploration) and optimal_path:
-        ax.scatter([node[0] for node in optimal_path], [node[1] for node in optimal_path], color='red', s=1)
+    total_frames = len(exploration) // exploration_step_size
 
-total_frames = len(exploration) // exploration_step_size 
+    ani = FuncAnimation(fig, animate, frames=total_frames, interval=0, repeat=False)
 
-ani = FuncAnimation(fig, animate, frames=total_frames, interval=0, repeat=False)
-
-
-plt.show()
-    
+    plt.show()
